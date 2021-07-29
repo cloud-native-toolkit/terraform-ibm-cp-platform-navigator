c06b57b56da54dc0a2e91b7e4ab7dc11c4a9142a
Sean Sundberg <seansund@us.ibm.com>
2021-07-29 11:01:06 -0500

    Updates test values
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-platform-navigator.tf
+++ test/stages/stage2-platform-navigator.tf
@@ -2,7 +2,7 @@ module "platform-navigator" {
   source = "./module"
 
   catalog_name        = module.cp4i.name
-  cluster_type        = module.dev_cluster.type_code
+  cluster_type        = module.dev_cluster.platform.type_code
   cluster_config_file = module.dev_cluster.config_file_path
   namespace           = module.dev_capture_tools_state.namespace
 }
